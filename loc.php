c01942509de4c2abeecdb1100274c96ad6a20649
onionwyl <362919284@qq.com>
2015-10-23 12:38:10 +0800

    最初首页框架
    简单首页框架

---
+++ loc.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- html document -->
+    <!--<meta name="viewport"-->
+    <!--content="-->
+    <!--height = [pixel_value | device-height] ,-->
+    <!--width = [pixel_value | device-width ] ,-->
+    <!--initial-scale = float_value ,-->
+    <!--minimum-scale = float_value ,-->
+    <!--maximum-scale = float_value ,-->
+    <!--user-scalable = [yes | no] ,-->
+    <!--target-densitydpi = [dpi_value | device-dpi | high-dpi | medium-dpi | low-dpi]-->
+    <!--"-->
+    <!--/>-->
+    <title>无标题文档</title>
+
+    <link rel="stylesheet" href="loc.css" type="text/css">
+
+    <style>
+        span {
+            /*position: absolute;*/
+            margin-top: 0%;
+            font-size: 1.5vw;
+
+        }
+
+        table {
+            margin: auto;
+            padding: 0%;
+            width: 100%;
+            height: 100%;
+        }
+
+        tr {
+            height: auto;
+        }
+
+        .container #notice1 {
+            border: solid 1px black;
+            height: 30%;
+            width: 30%;
+            float: left;
+            margin: 2%;
+        }
+
+        .container #notice2 {
+            border: solid 1px black;
+            height: 30%;
+            width: 30%;
+            margin: 2% 1% 2% 0%;
+            float: left;
+        }
+
+        .container #calendar {
+            border: solid 1px black;
+            height: 30%;
+            width: 30%;
+            margin: 2% 2% 2% 0%;
+            float: right;
+        }
+
+        .container #paihang {
+            border: solid 1px black;
+            height: 60%;
+            width: 60%;
+            margin: 25% 1% 1% 2%;
+        }
+
+        .container #special {
+            border: solid 1px black;
+            height: 27%;
+            width: 30%;
+            float: right;
+            position: relative;
+            margin: -43% 2% 0% 1%;
+        }
+
+        .container #mail {
+            border: solid 1px black;
+            height: 10%;
+            width: 30%;
+            float: right;
+            position: relative;
+            margin: -23% 2% 1% 1%;
+        }
+
+        .container #depart {
+            border: solid 1px black;
+            height: 20%;
+            width: 30%;
+            float: right;
+            position: relative;
+            margin: -15% 2% 1% 1%;
+        }
+
+        .log {
+            border: solid 1px black;
+            height: 15%;
+            width: 98%;
+            margin: 1%;
+        }
+
+        .banner {
+            border: solid 1px black;
+            height: 70%;
+            width: 98%;
+            margin: 1%;
+        }
+
+        /*two begin */
+        .nav {
+            border: solid 1px black;
+            height: 8%;
+            width: 98%;
+            margin: 1%;
+        }
+
+        .nav td {
+            text-align: center;
+        }
+
+        .notice {
+            border: solid 1px black;
+            height: 6%;
+            width: 98%;
+            margin: 1%;
+        }
+
+        .content .two .context {
+            border: solid 1px black;
+            height: 48%;
+            width: 98%;
+            margin: 1%;
+        }
+
+        .container .two .link {
+            border: solid 1px black;
+            height: 25%;
+            width: 98%;
+            margin: 1%;
+        }
+
+        .two .left {
+            /*border: solid 1px black;*/
+            height: 100%;
+            width: 40%;
+            /*margin: 1%;*/
+            float: left;
+        }
+
+        .two .right {
+            border: solid 1px black;
+            height: 90%;
+            width: 55%;
+            margin: 1%;
+            float: left;
+        }
+
+        .four .left {
+            border: solid 1px black;
+            height: 90%;
+            width: 20%;
+            margin: 1% 0% 1% 1%;
+            float: left;
+        }
+
+        .four .right {
+            border: solid 1px black;
+            height: 90%;
+            width: 72%;
+            margin: 1%;
+            float: left;
+        }
+    </style>
+</head>
+
+<body>
+<div class="container">
+    <div class='dummy'></div>
+    <div class="content">
+        <div class="one">
+            <div class="log">
+                <span>用户名：123456789 密码：123123 验证码：123 登录</span>
+            </div>
+            <div class="banner">
+                <span>先锋网</span>
+            </div>
+        </div>
+        <div class="two">
+            <div class="nav">
+
+                <table border="0px" cellspacing="0">
+                    <tr>
+                        <td><a href="" style="text-decoration: none"><span>首页</span></a></td>
+                        <td><a href="" style="text-decoration: none"><span>东大共青团</span></a></td>
+                        <td><a href="" style="text-decoration: none"><span>素质拓展</span></a></td>
+                        <td><a href="" style="text-decoration: none"><span>网上团支部</span></a></td>
+                        <td><a href="" style="text-decoration: none"><span>青年组织</span></a></td>
+                        <td><a href="" style="text-decoration: none"><span>先锋网络</span></a></td>
+                        <td><a href="" style="text-decoration: none"><span>文件下载</span></a></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="notice">
+                <table border="0px" cellspacing="0">
+                    <tr>
+                        <td style="padding: 0%;margin: -1%;font-size: 1.5vw">欢迎来到东北大学团委先锋网</td>
+                        <td>
+                            <input type="submit" value="提交"
+                                   style="float: right; width: 4vw;height: 2vw; font-size: 1.1vw">
+                            <input type="text" placeholder="请输入关键词"
+                                   style="float: right;width: 14vw;height: 1.2vw;font-size: 1.1vw">
+                        </td>
+                    </tr>
+
+                </table>
+            </div>
+            <div class="context">
+                <div class="left">
+                    <table border="1px">
+                        <tr>
+                            <td colspan="1" rowspan="3" height="70%" width="70%">
+                                <span>图片</span>
+                            </td>
+                            <td height="20%" width="20%">
+                                <span>图片</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span>图片</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span>图片</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><span>description</span></td>
+                            <td><a style="text-decoration: none" href="#"><span>更多></span></a></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="right">
+                    <div style="height: 35%;width: 98%;border: solid 1px black;margin: 1%;">
+                        <span>头条新闻</span>
+                    </div>
+                    <div style="height: 55%;width: 98%;border: solid 1px black;margin: 1%;">
+                        <ul style="list-style: none">
+                            <li><span>新闻</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="link">
+                <span>some link</span>
+            </div>
+        </div>
+        <div class="three">
+            <!--这里面内容较多，不一一列举div，自行设计-->
+            <div id="notice1"><span>最新公告</span></div>
+            <div id="notice2"><span>最新公告</span></div>
+            <div id="calendar"><span>先锋校历</span></div>
+            <div id="paihang">
+                <span>排行榜</span>
+
+                <div id="spe1"></div>
+                <div id="spe2"></div>
+                <div id="spe3"></div>
+                <div id="spe4"></div>
+                <div id="spe5"></div>
+            </div>
+            <div id="special"><span>专题信息</span></div>
+            <div id="mail"><span>书记信箱</span></div>
+            <div id="depart"><span>部门在线</span></div>
+        </div>
+        <div class="four">
+            <!--这里是最简单的部分，自行设计-->
+            <div class="left">
+                <span>辽ICP备05001332号</span>
+            </div>
+
+            <div class="right">
+                <span><a rel="nofollow" href="http://bbs.neupioneer.com/forumdisplay.php?fid=173">建议意见</a></span> |
+                <span><a href="./neup" target="_blank">管理入口</a></span> |
+                <span><a href="/html/copyright.html">版权声明</a></span>
+                <span class="friendLinks"><a rel="nofollow" href="http://www.ahandbagsshop.com/best_deal.html"
+                                             class="friendLinks">handbags shop online</a>
+                </span><br>
+                <span class="copyright">
+                    Copyright1 &copy; 2002-2015 neupioneer.com 版权所有 All Rights Reserved. <br/>
+                </span>
+
+                <div class="link">
+                    <span>
+                    <strong>主办单位</strong>：<strong>共青团东北大学委员会</strong> &#160;<strong>技术支持</strong>：<a rel="nofollow"
+                                                                                                     href="http://tech.neupioneer.com/"><strong>先锋网技术部</strong></a>
+                    <a href="http://www.neupioneer.com/neup" target="_blank">管理入口</a> <br/>
+                    联系方式：Tel: 024-836(88321) Fax: 024-83687349 Email: <a
+                            href="mailto:neupioneer@126.com">neupioneer@126.com</a>
+                        </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
