2c36e92795bf4caa46bc5971598d4636b1cfaa0b
zero <1162698286@qq.com>
2015-11-12 15:31:29 +0800

    change the style by the last design

--- new_model/mod/common.js
+++ new_model/mod/common.js
@@ -1,21 +1,56 @@
-/*
- Copyright (c) 2012-2013, loyjers@qq.com. All rights reserved.
- 全局类：常用方法
- */
-
+$(function () {
+    //工作聚焦地方传真
+    C.neup_news({
+        "params": [
+            {"nav": "#neup_news_1", "con": "#neup_news_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#neup_news_2", "con": "#neup_news_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#neup_news_3", "con": "#neup_news_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+    C.mid_upl({
+        "params": [
+            {"nav": "#mid_upl_1", "con": "#mid_upl_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_upl_2", "con": "#mid_upl_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_upl_3", "con": "#mid_upl_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+    C.mid_upr({
+        "params": [
+            {"nav": "#mid_upr_1", "con": "#mid_upr_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_upr_2", "con": "#mid_upr_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_upr_3", "con": "#mid_upr_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+    C.mid_downl({
+        "params": [
+            {"nav": "#mid_downl_1", "con": "#mid_downl_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_downl_2", "con": "#mid_downl_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_downl_3", "con": "#mid_downl_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+    C.mid_downr({
+        "params": [
+            {"nav": "#mid_downr_1", "con": "#mid_downr_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_downr_2", "con": "#mid_downr_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#mid_downr_3", "con": "#mid_downr_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+    C.rank({
+        "params": [
+            {"nav": "#rank_1", "con": "#rank_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#rank_2", "con": "#rank_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#rank_3", "con": "#rank_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+    C.tabs({
+        "params": [
+            {"nav": "#tr_1", "con": "#tr_1con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#tr_2", "con": "#tr_2con", "sclass": "cur1", "nclass": "cur1_no"},
+            {"nav": "#tr_3", "con": "#tr_3con", "sclass": "cur1", "nclass": "cur1_no"}
+        ]
+    });
+});
 var C = {
-    /*
-     半透明提示层
-     支持：1）直接创建提示；2）页面内隐藏层变半透明显示
-     width 提示层宽度
-     height 提示层高度
-     title 提示层标题内容
-     content 提示层 提示内容
-     timeout 友情提示 （错误提示） 显示时间， top 友情提示 上边距
-     alert_style 设置 错误消息提示样式
-     border 设置弹窗层边框粗细
-     border_radius 设置边框圆角幅度
-     */
     "alert": {
         /*
          始终显示在屏幕中间 obj:始终居中的弹出层对象
@@ -33,7 +68,300 @@ var C = {
             $(window).bind('resize', setMask);
             $(window).bind('scroll', setMask);
         }
-
+    },
+    "neup_news": function (__params) {
+        //默认选中
+        var selected = __params.selected;
+        if (__params.selected) {
+            selected = __params.selected
+        } else {
+            selected = __params.params[0].nav;
+        }
+        //切换动作
+        var event = __params.event;
+        if (__params.event) {
+            event = __params.event
+        } else {
+            event = 'click';
+        }
+        //默认样式选中和不选中
+        if (!__params.style) __params.style = {"sclass": "selected", "nclass": "selected_no"};
+        //切换卡参数
+        var params = __params.params;
+        //遍历切换卡参数
+        for (var i = 0; i < params.length; i++) {
+            var tab = params[i];
+            //选项卡自定义了样式
+            var sclass = __params.style.sclass;
+            if (tab.sclass) sclass = tab.sclass;
+            var nclass = __params.style.nclass;
+            if (tab.nclass) nclass = tab.nclass;
+            //判断选中选项卡
+            if (selected == tab.nav) {
+                $(tab.nav).removeClass(nclass);
+                $(tab.nav).addClass(sclass);
+                $(tab.con).css({'display': ''});
+            } else {
+                $(tab.nav).removeClass(sclass);
+                $(tab.nav).addClass(nclass);
+                $(tab.con).css({'display': 'none'});
+            }//alert(event);
+            //绑定事件
+            $(tab.nav).unbind(event);
+            $(tab.nav).bind(event, function () {
+                C.tabs({
+                    "selected": "#" + $(this).attr('id'),
+                    "event": event,
+                    "style": __params.style,
+                    "params": params
+                });
+            });
+        }
+    },
+    "mid_upl": function (__params) {
+        //默认选中
+        var selected = __params.selected;
+        if (__params.selected) {
+            selected = __params.selected
+        } else {
+            selected = __params.params[0].nav;
+        }
+        //切换动作
+        var event = __params.event;
+        if (__params.event) {
+            event = __params.event
+        } else {
+            event = 'click';
+        }
+        //默认样式选中和不选中
+        if (!__params.style) __params.style = {"sclass": "selected", "nclass": "selected_no"};
+        //切换卡参数
+        var params = __params.params;
+        //遍历切换卡参数
+        for (var i = 0; i < params.length; i++) {
+            var tab = params[i];
+            //选项卡自定义了样式
+            var sclass = __params.style.sclass;
+            if (tab.sclass) sclass = tab.sclass;
+            var nclass = __params.style.nclass;
+            if (tab.nclass) nclass = tab.nclass;
+            //判断选中选项卡
+            if (selected == tab.nav) {
+                $(tab.nav).removeClass(nclass);
+                $(tab.nav).addClass(sclass);
+                $(tab.con).css({'display': ''});
+            } else {
+                $(tab.nav).removeClass(sclass);
+                $(tab.nav).addClass(nclass);
+                $(tab.con).css({'display': 'none'});
+            }//alert(event);
+            //绑定事件
+            $(tab.nav).unbind(event);
+            $(tab.nav).bind(event, function () {
+                C.tabs({
+                    "selected": "#" + $(this).attr('id'),
+                    "event": event,
+                    "style": __params.style,
+                    "params": params
+                });
+            });
+        }
+    },
+    "mid_upr": function (__params) {
+        //默认选中
+        var selected = __params.selected;
+        if (__params.selected) {
+            selected = __params.selected
+        } else {
+            selected = __params.params[0].nav;
+        }
+        //切换动作
+        var event = __params.event;
+        if (__params.event) {
+            event = __params.event
+        } else {
+            event = 'click';
+        }
+        //默认样式选中和不选中
+        if (!__params.style) __params.style = {"sclass": "selected", "nclass": "selected_no"};
+        //切换卡参数
+        var params = __params.params;
+        //遍历切换卡参数
+        for (var i = 0; i < params.length; i++) {
+            var tab = params[i];
+            //选项卡自定义了样式
+            var sclass = __params.style.sclass;
+            if (tab.sclass) sclass = tab.sclass;
+            var nclass = __params.style.nclass;
+            if (tab.nclass) nclass = tab.nclass;
+            //判断选中选项卡
+            if (selected == tab.nav) {
+                $(tab.nav).removeClass(nclass);
+                $(tab.nav).addClass(sclass);
+                $(tab.con).css({'display': ''});
+            } else {
+                $(tab.nav).removeClass(sclass);
+                $(tab.nav).addClass(nclass);
+                $(tab.con).css({'display': 'none'});
+            }//alert(event);
+            //绑定事件
+            $(tab.nav).unbind(event);
+            $(tab.nav).bind(event, function () {
+                C.tabs({
+                    "selected": "#" + $(this).attr('id'),
+                    "event": event,
+                    "style": __params.style,
+                    "params": params
+                });
+            });
+        }
+    },
+    "mid_downl": function (__params) {
+        //默认选中
+        var selected = __params.selected;
+        if (__params.selected) {
+            selected = __params.selected
+        } else {
+            selected = __params.params[0].nav;
+        }
+        //切换动作
+        var event = __params.event;
+        if (__params.event) {
+            event = __params.event
+        } else {
+            event = 'click';
+        }
+        //默认样式选中和不选中
+        if (!__params.style) __params.style = {"sclass": "selected", "nclass": "selected_no"};
+        //切换卡参数
+        var params = __params.params;
+        //遍历切换卡参数
+        for (var i = 0; i < params.length; i++) {
+            var tab = params[i];
+            //选项卡自定义了样式
+            var sclass = __params.style.sclass;
+            if (tab.sclass) sclass = tab.sclass;
+            var nclass = __params.style.nclass;
+            if (tab.nclass) nclass = tab.nclass;
+            //判断选中选项卡
+            if (selected == tab.nav) {
+                $(tab.nav).removeClass(nclass);
+                $(tab.nav).addClass(sclass);
+                $(tab.con).css({'display': ''});
+            } else {
+                $(tab.nav).removeClass(sclass);
+                $(tab.nav).addClass(nclass);
+                $(tab.con).css({'display': 'none'});
+            }//alert(event);
+            //绑定事件
+            $(tab.nav).unbind(event);
+            $(tab.nav).bind(event, function () {
+                C.tabs({
+                    "selected": "#" + $(this).attr('id'),
+                    "event": event,
+                    "style": __params.style,
+                    "params": params
+                });
+            });
+        }
+    },
+    "mid_downr": function (__params) {
+        //默认选中
+        var selected = __params.selected;
+        if (__params.selected) {
+            selected = __params.selected
+        } else {
+            selected = __params.params[0].nav;
+        }
+        //切换动作
+        var event = __params.event;
+        if (__params.event) {
+            event = __params.event
+        } else {
+            event = 'click';
+        }
+        //默认样式选中和不选中
+        if (!__params.style) __params.style = {"sclass": "selected", "nclass": "selected_no"};
+        //切换卡参数
+        var params = __params.params;
+        //遍历切换卡参数
+        for (var i = 0; i < params.length; i++) {
+            var tab = params[i];
+            //选项卡自定义了样式
+            var sclass = __params.style.sclass;
+            if (tab.sclass) sclass = tab.sclass;
+            var nclass = __params.style.nclass;
+            if (tab.nclass) nclass = tab.nclass;
+            //判断选中选项卡
+            if (selected == tab.nav) {
+                $(tab.nav).removeClass(nclass);
+                $(tab.nav).addClass(sclass);
+                $(tab.con).css({'display': ''});
+            } else {
+                $(tab.nav).removeClass(sclass);
+                $(tab.nav).addClass(nclass);
+                $(tab.con).css({'display': 'none'});
+            }//alert(event);
+            //绑定事件
+            $(tab.nav).unbind(event);
+            $(tab.nav).bind(event, function () {
+                C.tabs({
+                    "selected": "#" + $(this).attr('id'),
+                    "event": event,
+                    "style": __params.style,
+                    "params": params
+                });
+            });
+        }
+    },
+    "rank": function (__params) {
+        //默认选中
+        var selected = __params.selected;
+        if (__params.selected) {
+            selected = __params.selected
+        } else {
+            selected = __params.params[0].nav;
+        }
+        //切换动作
+        var event = __params.event;
+        if (__params.event) {
+            event = __params.event
+        } else {
+            event = 'click';
+        }
+        //默认样式选中和不选中
+        if (!__params.style) __params.style = {"sclass": "selected", "nclass": "selected_no"};
+        //切换卡参数
+        var params = __params.params;
+        //遍历切换卡参数
+        for (var i = 0; i < params.length; i++) {
+            var tab = params[i];
+            //选项卡自定义了样式
+            var sclass = __params.style.sclass;
+            if (tab.sclass) sclass = tab.sclass;
+            var nclass = __params.style.nclass;
+            if (tab.nclass) nclass = tab.nclass;
+            //判断选中选项卡
+            if (selected == tab.nav) {
+                $(tab.nav).removeClass(nclass);
+                $(tab.nav).addClass(sclass);
+                $(tab.con).css({'display': ''});
+            } else {
+                $(tab.nav).removeClass(sclass);
+                $(tab.nav).addClass(nclass);
+                $(tab.con).css({'display': 'none'});
+            }//alert(event);
+            //绑定事件
+            $(tab.nav).unbind(event);
+            $(tab.nav).bind(event, function () {
+                C.tabs({
+                    "selected": "#" + $(this).attr('id'),
+                    "event": event,
+                    "style": __params.style,
+                    "params": params
+                });
+            });
+        }
     },
     "tabs": function (__params) {
         //默认选中
@@ -83,26 +411,5 @@ var C = {
                 });
             });
         }
-    },
-    /*lib库中nbslide幻灯片组件增加功能，使用增加缩略图预览功能
-     在幻灯片下方循环输出幻灯广告缩略图 thumb，每个图片的元素加上 onclick="C.slide_click(obj,num)"
-     obj 为幻灯片容器标识如 #abc,.abc，num为图片序号，0,1,2,....，反序的
-     */
-    "nbslide": {
-        //缩略图点击显示对应幻灯
-        "thumb_click": function (obj, num) {
-            var btn = $(obj).find("#btnList li");
-            btn.each(function (index) {
-                if (index == num) {
-                    $(this).click();
-                }
-            });
-        },
-        //左右方向按钮控制上一个下一个幻灯
-        "pn_click": function (obj, pn) {
-            var btn = $(obj).find(".sliderBtn");
-            if (pn == 'prev') $(obj).find("#prevBtn").click();
-            if (pn == 'next') $(obj).find("#nextBtn").click();
-        }
     }
-}
+};
